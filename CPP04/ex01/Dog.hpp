d68f44df93c1b05a9690bf7d484bc2b66d1fa6b5
Victtor Eduardo Britto De Moraes <vbritto-@c1r7s5.42porto.com>
2025-02-14 18:51:25 +0000

    ok

--- CPP04/ex01/Dog.hpp
+++ CPP04/ex01/Dog.hpp
@@ -6,7 +6,7 @@
 /*   By: vbritto- <vbritto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 13:32:53 by vbritto-          #+#    #+#             */
-/*   Updated: 2025/02/13 16:02:54 by vbritto-         ###   ########.fr       */
+/*   Updated: 2025/02/14 17:14:28 by vbritto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 #define DOG_HPP
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Dog : public virtual Animal
 {
+    private:
+        Brain* brain;
     public:
         Dog();
         ~Dog();
@@ -24,6 +27,8 @@ class Dog : public virtual Animal
         Dog &operator=(const Dog &original);
 
         void makeSound()const;
+        void addIdea(std::string think);
+        void showInfo();
 };
 
 #endif
